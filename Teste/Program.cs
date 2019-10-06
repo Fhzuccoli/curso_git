f603d0bc7d14fccea42783cdad3dc5c8b66c1adf
Felipe Henrique <fhzuccoli@gmail.com>
2019-10-06 13:59:35 -0300

    Acrescentado Boa noiute

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -5,6 +5,7 @@ namespace Teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Bom noite");
         }
     }
 }
