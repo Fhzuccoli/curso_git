b7c73d82e73d0c804cdf250196b2da83536a6d94
Felipe Henrique <fhzuccoli@gmail.com>
2019-10-06 18:12:44 -0300

    Retirada a mensagem bom dia

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -4,7 +4,6 @@ namespace Teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
             Console.WriteLine("Bom noite");
         }
     }
